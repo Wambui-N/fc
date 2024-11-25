3c46ba68d3a7cd92855af1d96859fcabd5191da4
Wambui Ndungu <wambuin18@gmail.com>
2024-11-25 12:15:43 +0300

    customize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#010D0D", // Dark background color
+        foreground: "var(--foreground)", // Variable for foreground color
+        transparent: "transparent", // Transparent color
+        primary: "#29A8A8", // Main primary color
+        primaryLight: "#1B6E6E", // Lighter shade of primary
+        secondary: "#051D1D", // Secondary color
+        white: "#F2FFFF", // White color
+        // Consider using a scale for blue if you have multiple shades
+        blue: {
+          DEFAULT: "#1B6E6E", // Base blue color
+          light: "#29A8A8", // Lighter blue
+          dark: "#051D1D", // Darker blue
+        },
       },
     },
   },
